fbe0a47c5dff8c159570fab71e13165dfee3e17b
Adam Serruys <adam.serruys@lume.com>
2023-03-31 12:57:12 -0400

    More config changes for lsp and harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,3 +3,5 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>1", ui.nav_next)
+vim.keymap.set("n", "<leader>2", ui.nav_prev)
